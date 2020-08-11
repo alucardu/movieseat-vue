38bab8702916115683ae8f063ea140e3cd0ae00c
Peter Boomsma <peter.boomsma@wijzijnumbrella.nl>
2020-08-11 22:03:55 +0200

    Add functionality to search a movie title

---
+++ src/components/MovieSearch/MovieSearchResultList/MovieSearchResultList.vue
@@ -0,0 +1,80 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import MovieSearchResults from '../../../stores/MovieSearchList';
+
+type Movie = {
+  title: string;
+  id: number;
+}
+
+@Component
+export default class MovieSearchResultList extends Vue {
+  imagePath = 'https://image.tmdb.org/t/p/w45';
+
+  get searchList(): Movie[] {
+    return MovieSearchResults.state.searchList;
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+  #movieSearchResultList {
+    background: #3a3a3a;
+    position: absolute;
+    z-index: 1;
+    list-style: none;
+    top: 46px;
+    width: 50%;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    li {
+      padding: 0 0px 0 8px;
+      display: flex;
+      align-items: center;
+      position: relative;
+      height: 68px;
+      &:nth-child(even) {
+        background: #444444;
+      }
+      &:hover {
+        cursor: pointer;
+        background: #0fcece;
+      }
+      p {
+        width: calc(98% - 45px);
+        display: flex;
+        flex-direction: column;
+        span:nth-child(1) {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+        span:nth-child(2) {
+          font-size: 12px;
+          color: #929292;
+        }
+      }
+      img {
+        margin-left: auto;
+        width: 45px;
+        height: 100%;
+      }
+    }
+  }
+</style>
+
+<template>
+  <ul id='movieSearchResultList'>
+    <li v-for='movie in searchList' :key='movie.id'>
+      <p>
+        <span>{{ movie.original_title }}</span>
+        <span>{{ movie.release_date }}</span>
+      </p>
+      <img :src="movie.poster_path
+        ? `${imagePath + movie.poster_path}`
+        : 'poster_not_found.svg'" />
+    </li>
+  </ul>
+</template>
---
+++ src/stores/MovieSearchList.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { orderBy } from 'lodash';
+
+Vue.use(Vuex);
+
+type Movie = {
+  title: string;
+  release_date: string;
+  id: number;
+}
+
+export default new Vuex.Store({
+  state: {
+    searchList: [] as Movie[],
+  },
+  mutations: {
+    createList(state, payload) {
+      state.searchList = orderBy(payload.results, [(movie: Movie) => movie.release_date], ['desc']);
+    },
+  },
+  getters: {
+    list(state) {
+      return state.searchList;
+    },
+  },
+});
