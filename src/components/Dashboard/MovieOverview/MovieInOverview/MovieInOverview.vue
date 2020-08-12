3942e498e945b6adeb92190dc695e86531579e0e
Peter Boomsma <peter.boomsma@wijzijnumbrella.nl>
2020-08-12 16:06:41 +0200

    Improved dashboard display

---
+++ src/components/Dashboard/MovieOverview/MovieInOverview/MovieInOverview.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+type Movie = {
+  title: string;
+  release_date: string;
+  id: number;
+  backdrop_path: string;
+}
+
+@Component
+export default class MovieInOverview extends Vue {
+  @Prop() readonly movie!: Movie;
+
+  imagePath = 'https://image.tmdb.org/t/p/w185/';
+}
+</script>
+
+<style scoped lang="scss">
+</style>
+
+<template>
+  <div><img :src="imagePath + movie.poster_path" /></div>
+</template>
