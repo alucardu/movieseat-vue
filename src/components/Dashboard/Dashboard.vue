b00cf92e10f8d4671a9ae84701ca44804cf19c01
alucardu <peter@cleanpixel.nl>
2020-09-25 12:04:51 +0200

    Refactored dashboard component

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { defineComponent } from '@vue/composition-api';
 import MovieOverview from './MovieOverview/MovieOverview.vue';
 
-@Component({
+export default defineComponent({
+  name: 'Dashboard',
   components: {
     MovieOverview,
   },
-})
-export default class Dashboard extends Vue {}
+});
 </script>
 
 <style scoped lang="scss">
